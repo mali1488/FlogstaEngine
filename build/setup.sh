80c49d26bae814d223ddf0ca6f3a536ae99f027a
mali1488 <mattias.linder87@gmail.com>
2016-02-19 18:40:24 +0100

    removed install from cmake

--- build/setup.sh
+++ build/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -r CMake*
+rm -r src/
+rm cmake*
+rm FE
+rm Makefile
 cmake ..
 make
-make install
-printf "Trying to run FlogstEngine FE ...\n"
+printf "Trying to run FlogstEngine FE ...\n\n"
 ./FE
