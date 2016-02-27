a2267a3c7233b2a92d62191c503cccf17a772e9c
Simon Bergström <simon@menuan.se>
2016-02-27 18:37:57 +0100

    Hmm

--- src/FlogstaEngine.cpp
+++ src/FlogstaEngine.cpp
@@ -85,8 +85,8 @@ bool FlogstaEngine::init() {
   }
     
   SDL_GLContext context = NULL;
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+  // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   // SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
     
